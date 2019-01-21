6736aea523ce37ee43de6a7abc8dc4981729cfaf
elliotsymons13 <elliotsymons13@gmail.com>
2019-01-21 11:22:15 +0000

    App checks for permissions on startup (wifi and location).

--- app/src/main/java/com/example/elliotsymons/positioningtestbed/MainActivity.java
+++ app/src/main/java/com/example/elliotsymons/positioningtestbed/MainActivity.java
@@ -1,39 +1,95 @@
 package com.example.elliotsymons.positioningtestbed;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.wifi.ScanResult;
-import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.elliotsymons.positioningtestbed.R;
-
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
     private static final String TAG = "MainActivity";
 
+    private static final int PERMISSIONS_RQ_WIFI_STATE = 1; //TODO what value?
+    private static final int PERMISSIONS_RQ_FINE_LOCATION = 2;
+
     WifiManager wifiManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Checking for wifi permission
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_WIFI_STATE) != PackageManager.PERMISSION_GRANTED) {
+            //If permission is NOT granted, request it:
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_WIFI_STATE},
+                    PERMISSIONS_RQ_WIFI_STATE);
+        } else {
+            Toast.makeText(this, "Permission wifi already got", Toast.LENGTH_SHORT).show();//else, the permission is already granted...
+        }
 
+        //Set up wifi manager
         wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
         enableWifi();
         registerReceiver(wifiScanReciever,
                 new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
 
+        //Check for location permission
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //If permission is NOT granted, request it:
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_RQ_FINE_LOCATION);
+        } else {
+            Toast.makeText(this, "Permission location already got", Toast.LENGTH_SHORT).show();//else, the permission is already granted...
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_RQ_WIFI_STATE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //permission was granted
+                    Toast.makeText(this, "Wifi permission granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    //permission was not granted 
+                    Toast.makeText(this, "Wifi permission required for app to function", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+            case PERMISSIONS_RQ_FINE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //permission was granted
+                    Toast.makeText(this, "Location permission granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    //permission was not granted
+                    Toast.makeText(this, "Location permission required for app to function", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+        }
     }
 
     private final BroadcastReceiver wifiScanReciever = new BroadcastReceiver() {
