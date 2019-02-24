844ff031a0de0e936a28c98800be5660003e3bf7
elliotsymons13 <elliotsymons13@gmail.com>
2019-02-24 16:41:07 +0000

    IntentService toasts scan results to UI thread for testing.

--- app/src/main/java/com/example/elliotsymons/positioningtestbed/WiFiFingerprintManagement/FingerprintingIntentService.java
+++ app/src/main/java/com/example/elliotsymons/positioningtestbed/WiFiFingerprintManagement/FingerprintingIntentService.java
@@ -2,22 +2,35 @@ package com.example.elliotsymons.positioningtestbed.WiFiFingerprintManagement;
 
 import android.app.IntentService;
 import android.app.Notification;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.elliotsymons.positioningtestbed.R;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static com.example.elliotsymons.positioningtestbed.App.CHANNEL_ID;
 
 public class FingerprintingIntentService extends IntentService {
     private static final String TAG = "Fingerpr.IntentServ";
+
+    WifiManager wifiManager;
 
     public FingerprintingIntentService() {
         super(TAG);
@@ -29,10 +42,10 @@ public class FingerprintingIntentService extends IntentService {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.i(TAG, "onCreate: Creating notification for IntentService");
 
         //Notification must be shown on oreo and higher (API 26+) to maintain service
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Log.i(TAG, "onCreate: Creating notification for IntentService");
             Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
                     .setContentTitle("WiFi Fingerprinting Service")
                     .setContentText("Running...")
@@ -41,15 +54,39 @@ public class FingerprintingIntentService extends IntentService {
 
             startForeground(1, notification);
         }
+
+        //Set up wifi manager
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        enableWifi();
+        registerReceiver(wifiScanReceiver,
+                new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+
+    }
+
+    public void postToastMessage(final String message) {
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        handler.post(new Runnable() {
+
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
         Log.d(TAG, "onHandleIntent: called");
 
+        SystemClock.sleep(2000);
+
         //TODO
 
         //TODO get scan result (see POC earlier on)
+        Log.i(TAG, "onHandleIntent: Requesting scan");
+        getWifiInfo();
+
         //TODO extract needed values
         //TODO pass to fingerprint manager
 
@@ -64,12 +101,68 @@ public class FingerprintingIntentService extends IntentService {
 
         //TODO ...
 
-        Log.d(TAG, "onHandleIntent: Finished waiting");
+        Log.i(TAG, "onHandleIntent: Finished waiting");
     }
+
+    private final BroadcastReceiver wifiScanReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
+                boolean success = intent.getBooleanExtra(
+                        WifiManager.EXTRA_RESULTS_UPDATED, false);
+                if (success) {
+                    onScanSuccess();
+                } else {
+                    onScanFailure();
+                }
+            }
+        }
+    };
+
+    /**
+     * Enable WiFi if not already enabled.
+     */
+    private void enableWifi() {
+        if (!wifiManager.isWifiEnabled()) {
+            wifiManager.setWifiEnabled(true);
+        }
+    }
+
+    private void onScanSuccess() {
+        Log.i(TAG, "onScanSuccess: Scan result received");
+        List<ScanResult> scanResults = wifiManager.getScanResults();
+        postToastMessage("Scan completed");
+
+        //Process results:
+        //TextView tv = getApplicationContext().findViewById(R.id.tv_info);
+        String text = "";
+        for (ScanResult result : scanResults) {
+            text += "SSID: " + result.SSID + "\n";
+            text += "MAC : " + result.BSSID + "\n";
+            text += "RSSI: " + result.level + "\n";
+            postToastMessage(text);
+        }
+        //tv.setText(text);
+
+    }
+
+    private void onScanFailure() {
+        postToastMessage("Scan failed");
+    }
+
+    /**
+     * Force a WiFi scan, if allowed by the OS.
+     */
+    public void getWifiInfo() {
+        wifiManager.startScan();
+    }
+
+
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        unregisterReceiver(wifiScanReceiver);
         Log.d(TAG, "onDestroy: IntentService closed");
     }
 }
